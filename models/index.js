05edfeb512e743548fc334638e640ab48acedcfc
Adam Wheeler <farfromruin@aol.com>
2020-02-17 11:00:29 -0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -8,9 +8,6 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-If (process.env.JAWSDB_URL) {
-   Connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
